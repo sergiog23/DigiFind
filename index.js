cfa26200cd41bfd1edcfdd68e75f770195ef3966
eeshaan <me@eeshaan.life>
2019-11-10 02:45:31 -0600

    Start masonry grid

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ $('.btn-selector').click(function(e) {
   e.preventDefault();
   $('.have-container').toggle()
 })
+
+$('.grid').masonry({
+  // options...
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
