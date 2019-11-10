3abed7fb6d66af3765e9715bfafb057b333a5a5c
Sergio Guerrero <sergiog0698@gmail.com>
2019-11-10 10:11:18 -0600

    e

--- app.js
+++ app.js
@@ -2,22 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
 
-var urlencodedParser = bodyParser.urlencoded({extended:false})
-
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.get('/submit-form',function(req,res){
-  res.render('submit-form',{qs:req.query});
-});
-
-app.post('/submit-form',urlencodedParser,function(req,res){
-  console.log(req.body);
-  res.render('submit-form',{data: req.body});
-});
-
-app.listen(3000, function() {
-  console.log('server is running on port 3000')
+app.post('/submit-form', (req, res) => {
+  const item = req.body.item
+  //...
+  res.end()
 })
